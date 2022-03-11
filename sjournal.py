3e04cad01d4597076f308262c59d67d864e1e324
Samuel Stuver <samuelstuver@gmail.com>
2022-03-10 22:28:25 -0700

    fix getting notes/length in sjournal. journal.length is still really slow, need to look into

--- sjournal.py
+++ sjournal.py
@@ -278,20 +278,6 @@ class SJournal:
                 # print(note)
         self.show_print_table()
 
-    def fetch(self):
-        notes = []
-        if not self.connection:
-            self.create_connection()
-        cursor = self.connection.cursor()
-        query = "SELECT * FROM notes ORDER BY id DESC"
-        cursor.execute(query)
-
-        for item in cursor.fetchall():
-            note = Note(item[0], item[2], item[3], date_time=datetime.strptime(item[1], "%m-%d-%y %H:%M:%S"))
-            notes.append(note)
-
-        return notes
-
     def backup(self):
         backup_dir = os.path.join(self.root_dir, self.journal_dir, "backups", self.journal_name)
 
@@ -360,15 +346,28 @@ class SJournal:
         self.journal_name = config["journal_name"]
 
     @property
+    def notes(self):
+        return self._get_notes()
+
+    @property
     def length(self):
         return self._get_length()
 
-    @property
-    def notes(self):
-        return self.fetch()
-
     def _get_length(self):
         return len(self.notes)
+
+    def _get_notes(self):
+        self.create_connection()
+        query = "SELECT * FROM notes"
+        cursor = self.connection.cursor()
+        cursor.execute(query)
+        items = cursor.fetchall()
+        notes = []
+        for item in items:
+            notes.append(Note(item[0], item[2], item[3], date_time=datetime.strptime(item[1], "%m-%d-%y %H:%M:%S")))
+        self.close_connection()
+
+        return notes
 
 
 class Note:
