228cbd4890400270d06277766adc80b9252e6e25
Samuel Stuver <samuelstuver@gmail.com>
2022-03-14 22:22:30 -0600

    try removing git status

--- tests/test_commands.py
+++ tests/test_commands.py
@@ -209,7 +209,7 @@ def test_edit_note(fixed_notes_journal, environment):
         logger.info([sjournal_exec.split()[0], sjournal_exec.split()[1], 'edit', f'{note.id}'.strip()])
 
         proc = subprocess.Popen([sjournal_exec.split()[0], sjournal_exec.split()[1], 'edit', f'{note.id}'.strip()], stdout=subprocess.PIPE, stdin=subprocess.PIPE, stderr=subprocess.PIPE, shell=True)
-        result = proc.communicate(input=b'EDITED')
+        result = proc.communicate(input='EDITED')
 
         logger.debug(result)
 
