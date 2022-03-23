0361b6528debfd7afdeccecce408dc4b2e8c76e9
Samuel Stuver <samuelstuver@gmail.com>
2022-03-23 00:05:50 -0600

    possible syntax error with send_cli_command?

--- tests/utils_test.py
+++ tests/utils_test.py
@@ -125,8 +125,7 @@ def send_cli_command(commandline, assert_okay=True, user_input=None):
 
     logger.info(f"Sending command {commandline}")
     try:
-        result = subprocess.run(commandline, check=assert_okay, input=user_input,
-                                shell=False, capture_output=True, text=True)
+        result = subprocess.run(commandline, check=assert_okay, input=user_input, shell=False, capture_output=True, text=True)
     except subprocess.CalledProcessError as command_error:
         logger.info(f"RETURN CODE {command_error.returncode}")
         logger.debug(f"STDOUT: \n{command_error.stdout}")
